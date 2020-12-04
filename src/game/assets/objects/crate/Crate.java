5f570ac53fee93c570383a674ec76de2da5663c5
NielzosFilms <nielzosfilms@gmail.com>
2020-12-04 23:23:34 +0100

    added wooden axe to crate loot

--- src/game/assets/objects/crate/Crate.java
+++ src/game/assets/objects/crate/Crate.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 import game.assets.HealthBar;
 import game.assets.items.*;
+import game.assets.items.tools.wood.Tool_WoodenAxe;
 import game.assets.objects.rock.Item_Rock;
 import game.assets.objects.stick.Item_Stick;
 import game.system.audioEngine.AudioFiles;
@@ -46,10 +47,14 @@ public class Crate extends GameObject {
     private void generateLoot() {
         LinkedList<Item> items = new LinkedList<>();
         for(int i=0; i<rand.nextInt(3) + 1; i++) {
-            if(rand.nextInt(2) == 1) {
-                items.add(new Item_Stick(rand.nextInt(5) + 1));
+            if(rand.nextInt(100) < 1) {
+                items.add(new Tool_WoodenAxe());
             } else {
-                items.add(new Item_Rock(rand.nextInt(5) + 1));
+                if(rand.nextInt(2) == 1) {
+                    items.add(new Item_Stick(rand.nextInt(5) + 1));
+                } else {
+                    items.add(new Item_Rock(rand.nextInt(5) + 1));
+                }
             }
         }
         inv.fillRandom(items);
